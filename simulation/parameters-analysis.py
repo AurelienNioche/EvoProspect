c01e9e5ce911937b6bc49cb052aec98fdd71ed1e
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2020-06-09 18:37:03 +0200

    Add legend to parameters analysis

--- simulation/parameters-analysis.py
+++ simulation/parameters-analysis.py
@@ -35,7 +35,7 @@ median = np.median(score)
 C = plt.contour(score, levels=[median,],
                 extent=[pmin, pmax, vmin, vmax],
                 vmin=0, vmax=1, origin="lower", colors="black", zorder=50)
-ax.set_title("Mean gain of the luckiest individuals", weight="bold")
+ax.set_title("Mean behavior (α,β) of the final population", weight="bold")
 ax.set_xlabel("α (probability function)" )
 ax.set_ylabel("β (utility function)")
 
@@ -43,49 +43,56 @@ marker_size = 75
 
 data = parameters.default()
 locals().update(data)
-for selection_rate in np.arange(5,100,5)/100:
+for i, selection_rate in enumerate(np.arange(5,100,5)/100):
     p = "(selection_rate=%.2f,mutation_rate=%.2f,mixture_rate=%.2f)"
     p = p % (selection_rate, mutation_rate, mixture_rate)
     filename = "data/simulation-final-population" + p + ".npy"
     agent_f = np.load(filename)
     X, Y = agent_f[:,0], agent_f[:,1]
-    ax.scatter([X.mean()], [Y.mean()], s=marker_size, alpha=.5,
+    label = None
+    if i == 0: label="selection_rate"
+    ax.scatter([X.mean()], [Y.mean()], s=marker_size, alpha=.5, label=label,
                facecolor="black", edgecolor="white", marker="X", lw=1)
 
 
 data = parameters.default()
 locals().update(data)
-for mutation_rate in np.array([1,2,3,4,5,10,20,50])/100:
+for i,mutation_rate in enumerate(np.array([1,2,3,4,5,10,20,50])/100):
     p = "(selection_rate=%.2f,mutation_rate=%.2f,mixture_rate=%.2f)"
     p = p % (selection_rate, mutation_rate, mixture_rate)
     filename = "data/simulation-final-population" + p + ".npy"
     agent_f = np.load(filename)
     X, Y = agent_f[:,0], agent_f[:,1]
-    ax.scatter([X.mean()], [Y.mean()], s=marker_size, alpha=.5,
+    label = None
+    if i == 0: label="mutation rate"
+    ax.scatter([X.mean()], [Y.mean()], s=marker_size, alpha=.5, label=label,
                facecolor="red", edgecolor="white", marker="o", lw=1)
 
 data = parameters.default()
 locals().update(data)
-for mixture_rate in np.arange(5,50,5)/100:
+for i,mixture_rate in enumerate(np.arange(5,50,5)/100):
     p = "(selection_rate=%.2f,mutation_rate=%.2f,mixture_rate=%.2f)"
     p = p % (selection_rate, mutation_rate, mixture_rate)
     filename = "data/simulation-final-population" + p + ".npy"
     agent_f = np.load(filename)
     X, Y = agent_f[:,0], agent_f[:,1]
-    ax.scatter([X.mean()], [Y.mean()], s=marker_size, alpha=.5,
+    label = None
+    if i == 0: label="mixture rate"
+    ax.scatter([X.mean()], [Y.mean()], s=marker_size, alpha=.5,  label=label,
                facecolor="blue", edgecolor="white", marker="D", lw=1)
 
 ax.axhline(0, lw=0.75, ls="--", color="black")
 ax.axvline(1, lw=0.75, ls="--", color="black")
+ax.legend(frameon=False)
 
 plt.text(0.01, .75, "risk averse", rotation=90, transform=ax.transAxes,
          ha="left", va="center", fontsize="small")
 plt.text(0.01, .25, "risk seeking", rotation=90, transform=ax.transAxes,
          ha="left", va="center", fontsize="small")
-plt.text(0.25, 0.99, "risk seeking", rotation=0, transform=ax.transAxes,
-         ha="center", va="top", fontsize="small")
-plt.text(0.75, 0.99, "risk averse", rotation=0, transform=ax.transAxes,
-         ha="center", va="top", fontsize="small")
+plt.text(0.25, 0.01, "risk seeking", rotation=0, transform=ax.transAxes,
+         ha="center", va="bottom", fontsize="small")
+plt.text(0.75, 0.01, "risk averse", rotation=0, transform=ax.transAxes,
+         ha="center", va="bottom", fontsize="small")
 
 
 
